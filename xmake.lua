29b6a97e9c86a9ca70bd8cb97cb5388fa5e33340
Anton <anton.nikan@gmail.com>
2026-09-19 23:03:19 +0200

    Making build work with xmake.

--- xmake.lua
+++ xmake.lua
@@ -14,9 +14,15 @@ target("breakout-cppfront")
 
 set_kind("binary")
 add_includedirs("src", "sdk/include")
+add_rules("cppfront")
 add_files(
-	"src/*.cpp"
+	"src/*.cpp2",
+	"src/*.h2"
 )
+
+on_load(function (target)
+	target:add("includedirs", path.join(target:autogendir(), "src"))
+end)
 
 add_deps("sdk")
 add_packages("cppfront")
